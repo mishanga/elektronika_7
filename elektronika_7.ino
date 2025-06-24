833c08581ace7247d96f0adb83cd52549348858b
Mikhail Troshev <mishanga@yandex-team.ru>
2025-06-24 14:45:31 +0300

    update

--- elektronika_7.ino
+++ elektronika_7.ino
@@ -10,9 +10,9 @@ const char fullDateFormat[] = "DD.MM.YYYY hh:mm:ss";
 const char dateFormat[] = "DDMM";
 const char timeFormat[] = "hhmm";
 
-LedControl lc = LedControl(8, 10, 9, 1); // DIN, CLK, CS, num | layout: fc16
-LiquidCrystal_I2C lcd(0x3F, 16, 2);      // custom address!
-RTC_DS3231 rtc;
+LedControl lc = LedControl(8, 10, 9, 1);  // DIN, CLK, CS, num | layout: fc16
+LiquidCrystal_I2C lcd(0x3F, 16, 2);       // custom address!
+RTC_DS3231 rtc;                           // in real project using DS3231 instead of DS1307
 
 DateTime rtcNow;
 uint32_t curMillis = 0;
@@ -21,28 +21,9 @@ uint32_t lastSetStateChange = 0;
 uint8_t clockState = 0;
 uint8_t setState = 0;
 
-/*
-uint8_t matrix[10][3] = {
-  {31, 1, 7, 1, 7}, // 0
-  {7, 1, 7, 1, 7}, // 3
-};
-*/
+uint8_t matrixState[8] = { 60, 66, 165, 129, 165, 153, 66, 60 };  // ☺
 
-uint8_t matrix[8] = {0, 1, 6, 5, 4, 31, 255, 0};
-
-void printMatrix(uint8_t *m, const uint8_t len)
-{
-  m[0] = 100;
-
-  for (uint8_t i = 0; i < len; i++)
-  {
-    Serial.println(matrix[i], BIN);
-    // std::cout << std::endl;
-  }
-}
-
-void setup()
-{
+void setup() {
   Serial.begin(115200);
   lcd.begin(16, 2);
   rtc.begin();
@@ -56,13 +37,7 @@ void setup()
   lc.setIntensity(0, 10);
   lc.clearDisplay(0);
 
-  lc.setLed(0, 0, 0, true);
-  lc.setLed(0, 1, 1, true);
-  lc.setLed(0, 2, 2, true);
-  lc.setLed(0, 0, 1, true);
-  lc.setLed(0, 0, 2, true);
-  lc.setLed(0, 1, 0, true);
-  lc.setLed(0, 2, 0, true);
+  printMatrix();
 
   // 0: 31, 17, 31
   // 1: 0, 0 (8?), 31
@@ -70,84 +45,82 @@ void setup()
   // 3: 17, 21, 31
   // 4: 28, 4, 31
   // 8: 31, 21, 31
-
-  // Serial.print(rtc.getTemperature());
-  Serial.print("init done");
 }
 
-void loop()
-{
+void loop() {
   curMillis = millis();
 
   updateClockState();
 
+  // TODO: updateSetState();
+
   delay(100);
 }
 
-void updateClockState()
-{
-  switch (clockState % 3)
-  {
-  case 0:
-    showClock();
-    if (lastClockStateChange + showClockDelay < curMillis)
-    {
-      clockState++;
-      lastClockStateChange = curMillis;
-      showDate();
-    }
-    break;
-  case 1:
-    if (lastClockStateChange + showDateDelay < curMillis)
-    {
-      clockState++;
-      lastClockStateChange = curMillis;
-      showWeather();
-    }
-    break;
-  case 2:
-    if (lastClockStateChange + showWeatherDelay < curMillis)
-    {
-      clockState++;
-      lastClockStateChange = curMillis;
+void updateClockState() {
+  switch (clockState % 3) {
+    case 0:
       showClock();
-    }
-    break;
+      if (lastClockStateChange + showClockDelay < curMillis) {
+        clockState++;
+        lastClockStateChange = curMillis;
+        showDate();
+      }
+      break;
+    case 1:
+      if (lastClockStateChange + showDateDelay < curMillis) {
+        clockState++;
+        lastClockStateChange = curMillis;
+        showWeather();
+      }
+      break;
+    case 2:
+      if (lastClockStateChange + showWeatherDelay < curMillis) {
+        clockState++;
+        lastClockStateChange = curMillis;
+        showClock();
+      }
+      break;
   }
 }
 
-void showClock()
-{
+void showClock() {
   rtcNow = rtc.now();
   showData(rtcNow.toString(timeFormat), rtcNow.second() % 2 == 0);
 }
 
-void showDate()
-{
+void showDate() {
   rtcNow = rtc.now();
   showData(rtcNow.toString(dateFormat), true);
 }
 
-void showWeather()
-{
+void showWeather() {
+  int8_t curTemp = (int)rtc.getTemperature();
+  // 1) добавить 100 битовым сдвигом: -103 / 103
+  // 2) превратить в строку, удалив 1, добавив + и пробел: " -03" / " +03"
+  // TODO: use rtc.getTemperature()
+
   showData(" +23", false);
 }
 
-void showData(char str[4], bool showDot)
-{
-  lcd.setCursor(5, 1); // bottom right
+void showData(char str[4], bool showDot) {
+  // TODO: use led matrix
+  lcd.setCursor(5, 1);
   lcd.print('[');
   lcd.print(str[0]);
   lcd.print(str[1]);
-  if (showDot)
-  {
+  if (showDot) {
     lcd.print(".");
-  }
-  else
-  {
+  } else {
     lcd.print(" ");
   }
   lcd.print(str[2]);
   lcd.print(str[3]);
   lcd.print(']');
+}
+
+void printMatrix() {
+  for (uint8_t i = 0; i < 8; i++) {
+    lc.setRow(0, i, matrixState[i]);
+  }
 }
